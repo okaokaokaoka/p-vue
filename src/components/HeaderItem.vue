2e378aeae5df110de3aa77fa3ebf155002c677ca
岡田徹 <toruhandbell0424@gmail.com>
2018-09-13 17:49:50 +0900

    Add search icon

--- src/components/HeaderItem.vue
+++ src/components/HeaderItem.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="my-header">
     <v-navigation-drawer></v-navigation-drawer>
-    <v-toolbar><v-toolbar-title>徹子の部屋</v-toolbar-title></v-toolbar>
+    <v-toolbar color="green" dark >
+      <v-toolbar-title>徹子の部屋</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+    </v-toolbar>
   </div>
 </template>
 
